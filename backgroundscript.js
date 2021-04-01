7196857fd3d50b57912a12b3da84506524ab8459
Arnav <arnav.malviya@outlook.com>
2021-03-31 22:57:39 -0700

    commit

---
+++ backgroundscript.js
@@ -0,0 +1,1 @@
+console.log(document.getElementsByClassName("SinglePriceLarge__PriceRow-ijh2t7-0 dESKpj"))
--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+var currentNumber = 1000000;
+var lastUpdated = "5pm";
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('start').addEventListener('click', startScripts);
 });
@@ -6,64 +8,45 @@ function startScripts() {
     chrome.storage.sync.get('phone_number', function(data) {
         if (typeof data.phone_number === 'undefined') {
             var phone_number=document.getElementById("phone_number").value;
-            console.log(phone_number)
             if (phone_number.length > 0) {
                 chrome.storage.sync.set({"phone_number": phone_number}, function() {
-                    console.log('Phone number is set to ' + phone_number);
-                    getPrice(phone_number)
+                    sendSMS(phone_number)
                 });
             }
         } else {
-            getPrice(data.phone_number)
+            sendSMS(data.phone_number)
         }
     });
 }
 
-function getPrice(phone_number) {
-    //var currentPrice = document.getElementsByClassName("SinglePriceLarge__PriceRow-ijh2t7-0 dESKpj")[0].getElementsByTagName('span')[0].innerHTML;
-    //console.log(currentPrice)
-    console.log(phone_number)
-    var price = "";
-    const req = new XMLHttpRequest();
-    const baseUrl = "http://localhost:8080/text";
+function sendSMS(phone_number) {
+    var prevNumber = currentNumber;
+    while (currentNumber > 0) {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+                currentNumber = response.current;
+                lastUpdated = response.lastUp;
+            });
+        });
+        console.log(currentNumber)
+        if (currentNumber != prevNumber) {
+            var bodyVal = "{\"phone_number\" : \"" + phone_number + "\",\"numberInLine\" : \"" + currentNumber + "\", \"lastUpdated\" : \"" + lastUpdated + "\"}"
+            console.log(bodyVal)
+            const req = new XMLHttpRequest();
+            const baseUrl = "http://localhost:8080/text";
+            var myHeaders = new Headers();
+            myHeaders.append('Content-Type', 'application/json');
+            fetch("http://localhost:8080/text", {
+                method: 'POST',
+                mode: 'cors',
+                cache: 'no-cache',
+                headers : myHeaders,
+                body: bodyVal
+            }).then(function(response) {
 
-    var bodyVal = "{\"phone_number\" : \"" + phone_number + "\"}"
+            });
+        }
+        prevNumber = currentNumber;
+    }
 
-    var myHeaders = new Headers();
-    myHeaders.append('Content-Type', 'application/json');
-    
-
-    fetch("http://localhost:8080/text", {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        headers : myHeaders,
-        body: bodyVal
-    }).then(function(response) {
-        // check the response object for result
-        // ...
-    });
-    previousPlace = 0
-
-    // while(true) {
-    //     place = document.getElementsByClassName("SinglePriceLarge__PriceRow-ijh2t7-0 dESKpj")[0].getElementsByTagName('span')[0].innerHTML;
-    //    if (previousPlace != 0 && (previousPlace - place) >= 5000) {
-    //         fetch("http://localhost:8080/text", {
-    //             method: 'POST',
-    //             mode: 'cors',
-    //             cache: 'no-cache',
-    //             headers : myHeaders,
-    //             body: bodyVal
-    //         }).then(function(response) {
-    //             // check the response object for result
-    //             // ...
-    //         });
-    //    } 
-    //     if (currentPrice !== price) {
-    //         console.log(price);
-    //         currentPrice = price;
-    //     }
-    //     previousPlace = place
-    //     console.log("waiting");
-    // }
 }
