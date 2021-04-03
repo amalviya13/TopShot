84ffefb2642dd1a3cdf0c58cef7227fd13122406
Arnav <arnav.malviya@outlook.com>
2021-04-03 16:03:57 -0700

    Updates

--- contentscript.js
+++ contentscript.js
@@ -1,7 +1,11 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        // var currentNumber = document.getElementById("MainPart_lbUsersInLineAheadOfYou").innerText;
-        // var lastUpdated = document.getElementById("MainPart_lbLastUpdateTimeText").innerText;
-        //sendResponse({current: currentNumber, lastUp : lastUpdated})
-        sendResponse({current: "100", lastUp : "5pm"})
+        if (request.method == "getNumber") {
+            var currentNumber = document.getElementById("1MainPart_lbUsersInLineAheadOfYou").innerText;
+            //var lastUpdated = document.getElementById("MainPart_lbLastUpdateTimeText").innerText;
+            //sendResponse({current: currentNumber, lastUp : lastUpdated})
+            sendResponse({current: currentNumber, lastUp : "5pm"})
+            return true;
+        }
+        return false;
 });
--- popup.js
+++ popup.js
@@ -1,8 +1,39 @@
 var currentNumber = 1000000;
+var prevNumber = currentNumber;
 var lastUpdated = "5pm";
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('start').addEventListener('click', startScripts);
 });
+
+function sendSMS(phone_number) {
+    var inst = setInterval(function() {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {method: "getNumber"}, function(response) {
+                currentNumber = response.current;
+                lastUpdated = response.lastUp;
+            });
+        });
+        if (prevNumber - currentNumber > 5000) {
+            var bodyVal = "{\"phone_number\" : \"" + phone_number + "\",\"numberInLine\" : \"" + currentNumber + "\", \"lastUpdated\" : \"" + lastUpdated + "\"}"
+            console.log(bodyVal);
+            const req = new XMLHttpRequest();
+            const baseUrl = "http://localhost:8080/text";
+            var myHeaders = new Headers();
+            myHeaders.append('Content-Type', 'application/json');
+            fetch("http://localhost:8080/text", {
+                method: 'POST',
+                mode: 'cors',
+                cache: 'no-cache',
+                headers : myHeaders,
+                body: bodyVal
+            }).then(function(response) {
+
+            });
+        prevNumber = currentNumber;
+        }
+
+    }, 2500);
+}
 
 function startScripts() {
     chrome.storage.sync.get('phone_number', function(data) {
@@ -19,34 +50,33 @@ function startScripts() {
     });
 }
 
-function sendSMS(phone_number) {
-    var prevNumber = currentNumber;
-    while (currentNumber > 0) {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-                currentNumber = response.current;
-                lastUpdated = response.lastUp;
-            });
-        });
-        console.log(currentNumber)
-        if (currentNumber != prevNumber) {
-            var bodyVal = "{\"phone_number\" : \"" + phone_number + "\",\"numberInLine\" : \"" + currentNumber + "\", \"lastUpdated\" : \"" + lastUpdated + "\"}"
-            console.log(bodyVal)
-            const req = new XMLHttpRequest();
-            const baseUrl = "http://localhost:8080/text";
-            var myHeaders = new Headers();
-            myHeaders.append('Content-Type', 'application/json');
-            fetch("http://localhost:8080/text", {
-                method: 'POST',
-                mode: 'cors',
-                cache: 'no-cache',
-                headers : myHeaders,
-                body: bodyVal
-            }).then(function(response) {
+// function sendSMS(phone_number) {
+//     var prevNumber = currentNumber;
+//     while (currentNumber > 0) {
+//         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//             chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+//                 console.log(response.current)
+//                 currentNumber = response.current;
+//                 lastUpdated = response.lastUp;
+//             });
+//         });
+//         if (currentNumber != prevNumber) {
+//             var bodyVal = "{\"phone_number\" : \"" + phone_number + "\",\"numberInLine\" : \"" + currentNumber + "\", \"lastUpdated\" : \"" + lastUpdated + "\"}"
+//             const req = new XMLHttpRequest();
+//             const baseUrl = "http://localhost:8080/text";
+//             var myHeaders = new Headers();
+//             myHeaders.append('Content-Type', 'application/json');
+//             fetch("http://localhost:8080/text", {
+//                 method: 'POST',
+//                 mode: 'cors',
+//                 cache: 'no-cache',
+//                 headers : myHeaders,
+//                 body: bodyVal
+//             }).then(function(response) {
 
-            });
-        }
-        prevNumber = currentNumber;
-    }
+//             });
+//         }
+//         prevNumber = currentNumber;
+//     }
 
-}
+// }
